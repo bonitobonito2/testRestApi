e2d6d9d43f214b30df0212c306e6481e0c18f5a5
bonitobonito2 <zaal.tsagareli.1@btu.edu.ge>
2022-11-07 17:31:12 +0400

    fix

--- src/users/dtos/users.dto.ts
+++ src/users/dtos/users.dto.ts
@@ -1,7 +1,13 @@
+import { isString } from 'util';
+
+import { MinLength, IsEmail, IsString } from 'class-validator';
+
 export default class UserCreateDto {
-
+  @IsString()
+  @MinLength(5)
   password: string;
-
+  @IsString()
   userName: string;
+  @IsEmail()
   email: string = undefined;
 }
